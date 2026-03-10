e3bdd782e0b53d243edb6b9c850db820768f3487
Rishith Gandham <rishith.gandham@gmail.com>
2026-03-10 14:24:36 -0400

    created dashboard and sidebar

---
+++ frontend/src/components/app-sidebar.tsx
@@ -0,0 +1,84 @@
+import * as React from "react"
+import {
+  IconChartBar,
+  IconDashboard,
+  IconFileAi,
+  IconHelp,
+  IconInnerShadowTop,
+  IconSettings,
+} from "@tabler/icons-react"
+
+import { NavMain } from "@/components/nav-main"
+import { NavSecondary } from "@/components/nav-secondary"
+import { NavUser } from "@/components/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+import citlogo from '@/assets/images/citlogo.png'
+import { Separator } from "./ui/separator"
+
+
+
+const data = {
+  user: {
+    name: "Rishith Gandham",
+    email: "rishith@citid.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "#",
+      icon: IconDashboard,
+    },
+    {
+      title: "Apps",
+      url: "#",
+      icon: IconFileAi,
+    },
+  ],
+  navSecondary: [
+    {
+      title: "Settings",
+      url: "#",
+      icon: IconSettings,
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar  collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu >
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:p-1.5!"
+            >
+              <a href="#">
+                <img src={citlogo} alt="CIT Id" className="w-8 h-8 object-contain shrink-0" />
+                <span className=" font-bold tracking-wide text-lg">CIT ID</span>
+              </a>
+                
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
